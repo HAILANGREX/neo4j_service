f19a0542469f24be871384882a9d51536c874e89
HAILANGREX <932729322@qq.com>
2021-06-16 17:10:00 +0800

    test

--- src/main/java/neo4jserver/App.java
+++ src/main/java/neo4jserver/App.java
@@ -12,23 +12,25 @@ public class App
 {
     public static void main( String[] args )
     {
-//        SearchService searchService = new SearchService("bolt://localhost:7687","neo4j","wjlwch");
-//
-//        Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "wjlwch" ) );
-//        Session session = driver.session();
-////        session.run( "CREATE (a:Person {name: $name, title: $title})",parameters( "name", "San Zhang", "title", "person" ) );
-//        session.run( "MATCH (a:Person {name: $name1}),(b:Person {name: $name2}) create (a)-[r: hate]->(b) return r",parameters( "name1", "Jialin Wang", "name2", "San Zhang","relation", "hate" ) );
-//
-//        Result result = session.run( "MATCH (a:Person) WHERE a.name = 'Jialin Wang' " + "RETURN a.name AS name, a.title AS title",
-//                parameters( "name", "Jialin Wang" ) );
-//
-//        while ( result.hasNext() )
-//        {
-//            Record record = result.next();
-//            System.out.println( record.get( "title" ).asString() + " " + record.get( "name" ).asString() );
-//        }
-//        session.close();
-//        driver.close();
+
+
+        Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "wjlwch" ) );
+        Session session = driver.session();
+//        session.run( "CREATE (a:Person {name: $name, title: $title})",parameters( "name", "San Zhang", "title", "person" ) );
+        session.run( "MATCH (a:Person {name: $name1}),(b:Person {name: $name2}) create (a)-[r: hate]->(b) return r",parameters( "name1", "Jialin Wang", "name2", "San Zhang","relation", "hate" ) );
+
+        Result result = session.run( "MATCH (a:Person) WHERE a.name = 'Jialin Wang' " + "RETURN a.name AS name, a.title AS title",
+                parameters( "name", "Jialin Wang" ) );
+
+        while ( result.hasNext() )
+        {
+            Record record = result.next();
+            System.out.println( record.get( "title" ).asString() + " " + record.get( "name" ).asString() );
+        }
+        session.close();
+        driver.close();
+
+
         SearchService searchService = new SearchService("bolt://localhost:7687","neo4j","wjlwch");
         searchService.add_point();
         searchService.service_close();
